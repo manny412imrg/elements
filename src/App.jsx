ca0ab11794d2842c8a9e1e8ccb9940ffda54d3f7
Ivan Romero <manny.parte.2@gmail.com>
2023-09-27 01:33:21 -0400

    primer elemento

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='body'>
+      <div className="container">
+        <div className="circle outer"></div>
+        <div className="circle inner"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
